d376bf06460e8fa341daa32400261eb6936ae155
kayvan61 <kayvan.mansoor@gmail.com>
2022-12-18 20:54:45 -0600

    cleaned up arenas

--- cogs/arena.py
+++ cogs/arena.py
@@ -14,14 +14,14 @@ class Arena(commands.Cog):
 
     @commands.command(name="arenaStart", description='starts an arena game')
     async def arena_start(self, ctx):
-        logging.debug("arena game attempted by", str(ctx.author)+"!")
+        logging.debug(f"arena game attempted by {str(ctx.author)}!")
         if self.arena_ongoing:
-            await ctx.reply("arena game already ongoing. use $arena_join to join the game, or $arena_fight to conclude the ongoing game.")
+            await ctx.reply("arena game already ongoing. use $arenaJoin to join the game, or $arenaFight to conclude the ongoing game.")
             return
 
         self.arena_players.append(ctx.author)
         self.arena_ongoing = True
-        await ctx.reply(str(ctx.author)+" started an arena game! type $arena_join to join.")
+        await ctx.reply(str(ctx.author)+" started an arena game! type $arenaJoin to join.")
 
     @commands.command(name="arenaJoin", description='enters an arena game')
     async def arena_join(self, ctx):
@@ -35,7 +35,7 @@ class Arena(commands.Cog):
     @commands.command(name="arenaFight", description='decides the winner of an arena game')
     async def arena_fight(self, ctx):
         if not self.arena_ongoing:
-            ctx.reply("fuck off. $arena_start fag.")
+            ctx.reply("No arena started. use $arena_start.")
             return
 
         winner_idx = random.randint(0, len(self.arena_players)-1)
